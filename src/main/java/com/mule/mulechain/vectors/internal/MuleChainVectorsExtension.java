db22671a747ee0e33cadcb858264b2ed641aa117
Amir Khan <amir.khan@salesforce.com>
2024-08-14 13:59:06 +0200

    name updated

--- src/main/java/com/mule/mulechain/vectors/internal/MuleChainVectorsExtension.java
+++ src/main/java/com/mule/mulechain/vectors/internal/MuleChainVectorsExtension.java
@@ -12,8 +12,8 @@ import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
  * and sources are going to be declared.
  */
-@Xml(prefix = "mulechain-vectors")
-@Extension(name = "MuleChain Vectors")
+@Xml(prefix = "vectors")
+@Extension(name = "MAC Vectors")
 @Configurations(MuleChainVectorsConfiguration.class)
 @JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
 public class MuleChainVectorsExtension {
