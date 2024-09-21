e09cc9746d07e17fcae611fc129db0096ed13bb9
Suraj Kamal <surajkamal60@gmail.com>
2024-09-21 23:07:06 +0530

    Restrict model name display to match the selected embeddingProviderType configuration

--- src/main/java/com/mule/mulechain/vectors/internal/MuleChainVectorsParameterEModelNameProvider.java
+++ src/main/java/com/mule/mulechain/vectors/internal/MuleChainVectorsParameterEModelNameProvider.java
@@ -1,29 +1,53 @@
 package com.mule.mulechain.vectors.internal;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import org.mule.runtime.api.value.Value;
+import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.values.ValueBuilder;
 import org.mule.runtime.extension.api.values.ValueProvider;
 import org.mule.runtime.extension.api.values.ValueResolvingException;
 
 public class MuleChainVectorsParameterEModelNameProvider implements ValueProvider {
 
-  private static final Set<Value> VALUES_FOR = ValueBuilder.getValuesFor(
+  @Config
+  private MuleChainVectorsConfiguration configuration;
+
+  private static final Set<Value> VALUES_FOR_OPENAI = ValueBuilder.getValuesFor(
           "text-embedding-3-small",
           "text-embedding-3-large",
-          "text-embedding-ada-002",
-          "mistral-embed",
-          "all-minilm",
-          "nomic-embed-text",
-          "tiiuae/falcon-7b-instruct",
-          "sentence-transformers/all-MiniLM-L6-v2"
+          "text-embedding-ada-002"
+  );
+
+  private static final Set<Value> VALUES_FOR_MISTRAL_AI = ValueBuilder.getValuesFor(
+          "mistral-embed"
+  );
+
+  private static final Set<Value> VALUES_FOR_NOMIC = ValueBuilder.getValuesFor(
+          "nomic-embed-text"
+  );
+
+  private static final Set<Value> VALUES_FOR_HUGGING_FACE = ValueBuilder.getValuesFor(
+            "tiiuae/falcon-7b-instruct",
+            "sentence-transformers/all-MiniLM-L6-v2"
   );
 
   @Override
   public Set<Value> resolve() throws ValueResolvingException {
-
-
-    return VALUES_FOR;
+    String embeddingProviderType = configuration.getEmbeddingProviderType();
+    switch (embeddingProviderType) {
+      case "OPENAI":
+        return VALUES_FOR_OPENAI;
+      case "MISTRAL_AI":
+        return VALUES_FOR_MISTRAL_AI;
+      case "NOMIC":
+        return VALUES_FOR_NOMIC;
+      case "HUGGING_FACE":
+        return VALUES_FOR_HUGGING_FACE;
+      default:
+        return Collections.emptySet();
+    }
   }
 
 }
